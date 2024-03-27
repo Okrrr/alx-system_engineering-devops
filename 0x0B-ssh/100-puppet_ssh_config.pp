17820034f4250f9f1f52b9d717a52e2be3b03c60
Okrrr <brian.korir00@gmail.com>
2024-03-27 06:28:59 -0700

    using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# using puppet to make configuration changes
+
+file{ 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
+}
