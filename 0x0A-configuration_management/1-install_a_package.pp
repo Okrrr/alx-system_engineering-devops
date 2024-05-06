ca2f9f0443d73d707ac38982b353714484015e23
Okrrr <brian.korir00@gmail.com>
2024-05-06 03:00:12 -0700

    Installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# A puppet manifest installing flask v2.1.0 which is a package from pip3
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Also specifying the version of flask to install using the ensure attribute
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-
+  provider => pip3,
 }
